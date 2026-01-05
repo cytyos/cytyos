dbc90a56157329a3d0f3b7ede37a1d706f84de20
cytyos <marciodonda@gmail.com>
2026-01-05 11:16:20 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { Platform } from './pages/Platform';
-import { Footer } from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Root Route: Shows Landing Page */}
+        {/* Rota Raiz: Landing Page */}
         <Route path="/" element={<LandingPage />} />
         
-        {/* App Route: Shows Platform with Footer layout */}
-        <Route path="/app" element={
-          <div className="flex flex-col h-screen w-full bg-gray-900 text-white overflow-hidden">
-             {/* Main Content Area (Sidebar + Map) */}
-             <div className="flex-1 relative overflow-hidden">
-                <Platform />
-             </div>
-             
-             {/* Fixed Footer */}
-             <Footer />
-          </div>
-        } />
+        {/* Rota App: Plataforma */}
+        <Route path="/app" element={<Platform />} />
         
-        {/* Fallback Route */}
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
