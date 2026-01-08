097146c85b43341b2213f4e1590fa5cd8168b3c5
cytyos <marciodonda@gmail.com>
2026-01-08 15:29:43 -0300

    Updated mapStore.ts

--- src/stores/mapStore.ts
+++ src/stores/mapStore.ts
@@ -1,30 +1,29 @@
 import { create } from 'zustand';
 
 interface MapStore {
-  mapStyle: 'satellite' | 'streets' | 'dark';
-  setMapStyle: (style: 'satellite' | 'streets' | 'dark') => void;
+  mapInstance: any;
+  setMapInstance: (map: any) => void;
   
-  drawMode: 'simple_select' | 'draw_polygon' | 'static';
-  setDrawMode: (mode: 'simple_select' | 'draw_polygon' | 'static') => void;
-  
+  mapStyle: string;
+  setMapStyle: (style: string) => void;
+  drawMode: string;
+  setDrawMode: (mode: string) => void;
   flyToCoords: [number, number] | null;
   setFlyToCoords: (coords: [number, number] | null) => void;
-
   is3D: boolean;
   setIs3D: (is3D: boolean) => void;
 }
 
 export const useMapStore = create<MapStore>((set) => ({
-  // MUDANÇA AQUI: O padrão agora é 'streets' (Mapa Claro)
-  mapStyle: 'streets', 
-  setMapStyle: (style) => set({ mapStyle: style }),
+  mapInstance: null,
+  setMapInstance: (map) => set({ mapInstance: map }),
   
+  mapStyle: 'satellite',
+  setMapStyle: (style) => set({ mapStyle: style }),
   drawMode: 'simple_select',
   setDrawMode: (mode) => set({ drawMode: mode }),
-  
   flyToCoords: null,
   setFlyToCoords: (coords) => set({ flyToCoords: coords }),
-
   is3D: false,
-  setIs3D: (val) => set({ is3D: val }),
+  setIs3D: (is3D) => set({ is3D }),
 }));
